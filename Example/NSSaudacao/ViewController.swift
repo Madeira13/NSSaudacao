ed508882e81d077f98500af668e0d1434b4d85a7
Calebe <calebenp@gmail.com>
2018-01-16 22:09:38 -0200

    Initial commit

---
+++ Example/NSSaudacao/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NSSaudacao
+//
+//  Created by CalebeNP on 01/16/2018.
+//  Copyright (c) 2018 CalebeNP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
